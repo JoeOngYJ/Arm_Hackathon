f14ec0077a21285483d1d90323392b7f02832d22
Andrew Farquharson <af765@cam.ac.uk>
2024-08-22 13:09:27 +0100

    added async clock

--- task_four/fsm.v
+++ task_four/fsm.v
@@ -1,8 +1,12 @@
-module FSM(input [2:0] r,
-    output reg [2:0] g);
+module FSM( input [2:0] r,
+            input clk,
+            input resetn,
+            output reg [2:0] g);
+
     reg [1:0] state;
 
-    always @(r) begin
+    always @(posedge clk) begin
+        if (resetn==0) state = 2'b00;
         case(state)
             0: begin
                 if (r[0]==1)
@@ -12,22 +16,13 @@ module FSM(input [2:0] r,
                 else if (r[2]==1)
                     state = 2'b11;
             end
-            1: begin
-                if (r[0]==0)
-                    state = 2'b00;
-            end
-            2: begin
-                if (r[1]==0)
-                    state = 2'b00;
-            end
-            3: begin
-                if (r[2]==0)
-                    state = 2'b00;
-            end
+            1: if (r[0]==0) state = 2'b00;
+            2: if (r[1]==0) state = 2'b00;
+            3: if (r[2]==0) state = 2'b00;
         endcase
     end
 
-    always @(state) begin
+    always @(posedge clk) begin
         case(state)
             0: g = 3'b000;
             1: g = 3'b001;
